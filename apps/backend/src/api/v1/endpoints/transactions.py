345830a0b30da59016a0d66f1259b36096dabb30
rifa-456 <rerserve@hotmail.com>
2025-10-23 21:06:16 -0300

    docs(api): plan upcoming tasks for v1 endpoints
    Adds in-code TODOs to track required implementation details for the transactions and categories endpoints.

--- apps/backend/src/api/v1/endpoints/transactions.py
+++ apps/backend/src/api/v1/endpoints/transactions.py
@@ -9,6 +9,7 @@ from apps.backend.src.db.database.connection import get_db
 router = APIRouter()
 
 
+#TODO make category post and get endpoints
 @router.post("/", response_model=transaction_schema.Transaction)
 def create_new_transaction(
         transaction: transaction_schema.TransactionCreate,
@@ -20,6 +21,7 @@ def create_new_transaction(
     return crud_transaction.create_transaction(db=db, transaction=transaction)
 
 
+#TODO make this accept query params
 @router.get("/", response_model=List[transaction_schema.Transaction])
 def read_transactions(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     """
